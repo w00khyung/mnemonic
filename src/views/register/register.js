12fdc5fb72211aa311565d71a60493020d1c6004
project-mizzu <project.mizzu@gmail.com>
2022-05-31 10:47:34 +0900

    [FE] Feat: 회원가입에 전화번호, 주소 추가

--- src/views/register/register.js
+++ src/views/register/register.js
@@ -6,17 +6,60 @@ const fullNameInput = document.querySelector('#fullNameInput');
 const emailInput = document.querySelector('#emailInput');
 const passwordInput = document.querySelector('#passwordInput');
 const passwordConfirmInput = document.querySelector('#passwordConfirmInput');
+const receiverPhoneNumberInput = document.querySelector('#receiverPhoneNumber');
+const postalCodeInput = document.querySelector('#postalCode');
+const addressOneInput = document.querySelector('#address1');
+const addressTwoInput = document.querySelector('#address2');
+const searchAddressButton = document.querySelector('#searchAddressButton');
 const submitButton = document.querySelector('#submitButton');
 
-addAllElements();
+// addAllElements();
 addAllEvents();
 
 // html에 요소를 추가하는 함수들을 묶어주어서 코드를 깔끔하게 하는 역할임.
-async function addAllElements() {}
+// function addAllElements() {
+
+// }
 
 // 여러 개의 addEventListener들을 묶어주어서 코드를 깔끔하게 하는 역할임.
 function addAllEvents() {
+  searchAddressButton.addEventListener('click', findAddress);
   submitButton.addEventListener('click', handleSubmit);
+}
+
+// Daum api
+async function findAddress() {
+  new daum.Postcode({
+    oncomplete: function (data) {
+      let addr = '';
+      let extraAddr = '';
+
+      if (data.userSelectedType === 'R') {
+        addr = data.roadAddress;
+      } else {
+        addr = data.jibunAddress;
+      }
+
+      if (data.userSelectedType === 'R') {
+        if (data.bname !== '' && /[동|로|가]$/g.test(data.bname)) {
+          extraAddr += data.bname;
+        }
+        if (data.buildingName !== '' && data.apartment === 'Y') {
+          extraAddr +=
+            extraAddr !== '' ? ', ' + data.buildingName : data.buildingName;
+        }
+        if (extraAddr !== '') {
+          extraAddr = ' (' + extraAddr + ')';
+        }
+      } else {
+      }
+
+      postalCodeInput.value = data.zonecode;
+      addressOneInput.value = `${addr} ${extraAddr}`;
+      addressTwoInput.placeholder = '상세 주소를 입력해 주세요.';
+      addressTwoInput.focus();
+    },
+  }).open();
 }
 
 // 회원가입 진행
@@ -27,12 +70,17 @@ async function handleSubmit(e) {
   const email = emailInput.value;
   const password = passwordInput.value;
   const passwordConfirm = passwordConfirmInput.value;
+  const phoneNumber = receiverPhoneNumberInput.value;
+  const postalCode = postalCodeInput.value;
+  const addressOne = addressOneInput.value;
+  const addressTwo = addressTwoInput.value;
 
   // 잘 입력했는지 확인
   const isFullNameValid = fullName.length >= 2;
   const isEmailValid = validateEmail(email);
   const isPasswordValid = password.length >= 4;
   const isPasswordSame = password === passwordConfirm;
+  const isPhoneNumber = /^01([0|1|6|7|8|9])-?([0-9]{3,4})-?([0-9]{4})$/;
 
   if (!isFullNameValid || !isPasswordValid) {
     return alert('이름은 2글자 이상, 비밀번호는 4글자 이상이어야 합니다.');
@@ -46,18 +94,22 @@ async function handleSubmit(e) {
     return alert('비밀번호가 일치하지 않습니다.');
   }
 
-  // 회원가입 api 요청
-  try {
-    const data = { fullName, email, password };
-
-    await Api.post('/api/register', data);
-
-    alert(`정상적으로 회원가입되었습니다.`);
-
-    // 로그인 페이지 이동
-    window.location.href = '/login';
-  } catch (err) {
-    console.error(err.stack);
-    alert(`문제가 발생하였습니다. 확인 후 다시 시도해 주세요: ${err.message}`);
+  if (isPhoneNumber.test(phoneNumber) === false) {
+    return alert('휴대폰 번호를 입력해주세요.');
   }
+
+  // // 회원가입 api 요청
+  // try {
+  //   const data = { fullName, email, password, phoneNumber, postalCode, addressTwo };
+
+  //   await Api.post('/api/register', data);
+
+  //   alert(`정상적으로 회원가입되었습니다.`);
+
+  //   // 로그인 페이지 이동
+  //   window.location.href = '/login';
+  // } catch (err) {
+  //   console.error(err.stack);
+  //   alert(`문제가 발생하였습니다. 확인 후 다시 시도해 주세요: ${err.message}`);
+  // }
 }
