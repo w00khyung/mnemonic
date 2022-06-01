51fe06a7c14bd1f26678c399c981858aeec62380
ctd <qpflapffhs76@gmail.com>
2022-06-01 13:17:27 +0900

    [BE] Feat: 사용자의 특정 주문 내역 취소 API

--- src/db/models/order-model.js
+++ src/db/models/order-model.js
@@ -10,13 +10,21 @@ export class OrderModel {
   }
 
   async findById(user) {
-    const findUser = await Order.findOne({ user });
+    const findUser = await Order.find({ user });
     return findUser;
   }
 
   async findAll() {
     const orders = await Order.find({});
     return orders;
+  }
+
+  async deleteOrder(user, orderNum) {
+    const findDeleteOrder = await Order.find({ user }).skip(orderNum);
+    const willDeleteOrder = await Order.deleteOne({
+      _id: findDeleteOrder[0]._id.toString(),
+    });
+    return willDeleteOrder;
   }
 }
 
