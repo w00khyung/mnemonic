913c7ae5d2177df2788fdff45567ab3905a58762
ctd <qpflapffhs76@gmail.com>
2022-06-01 17:10:32 +0900

    [BE] Feat: 주문 내역 조회 API 사용 시 페이지 구성에 필요한 정보만 전송(날짜, 주문 정보, 상태)

--- src/services/order-service.js
+++ src/services/order-service.js
@@ -14,7 +14,15 @@ class OrderService {
   // 특정 사용자의 주문 내역 조회
   async getOrderList(user) {
     const findOrderList = await this.orderModel.findById(user);
-    return findOrderList;
+
+    // DB에 들어가있는 주문 정보에서 필요한 것만 필터링 (날짜, 주문 정보, 상태)
+    const newOrderList = [];
+    findOrderList.map((orderlist) => {
+      const { orderInfo, createdAt } = orderlist;
+      newOrderList.push({ orderInfo, createdAt });
+    });
+    console.log(newOrderList);
+    return newOrderList;
   }
 
   // 특정 사용자의 주문 내역 삭제
