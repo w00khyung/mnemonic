511cf296af84a7405a3d2fe4c2c9b2790e3d0e24
[SW2/부산]오원빈 <ohwon99@gmail.com>
2022-06-05 18:21:28 +0000

    [FE] Docs: 중북된 코드와 필요 없는 코드 제거

--- src/views/product-detail/product-detail.js
+++ src/views/product-detail/product-detail.js
@@ -67,17 +67,6 @@ const data = {
   quantity: productQuantity,
 };
 
-const today = new Date();
-
-const hours = today.getHours();
-const minutes = today.getMinutes();
-const seconds = today.getSeconds();
-const milliseconds = today.getMilliseconds();
-
-const num = `${hours}${minutes}${seconds}${milliseconds}`;
-
-const randomId = Math.floor(Math.random() * parseInt(num));
-
 const dataString = JSON.stringify(data);
 
 addCartBtn.addEventListener('click', handleSubmit);
@@ -93,9 +82,6 @@ function handleSubmit() {
   displayModal();
   localStorage.setItem(currentProductId, dataString);
 }
-const dataString = JSON.stringify(data);
-
-addCartBtn.addEventListener('click', handleSubmit);
 
 // open modal
 function displayModal() {
