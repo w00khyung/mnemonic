a56f60b68190ade3765a6648363c9a6e19ef44a9
ctd <qpflapffhs76@gmail.com>
2022-06-10 18:33:09 +0900

    [FE/BE] 소셜 로그인 (카카오) 구현

--- src/routers/social-login-router.js
+++ src/routers/social-login-router.js
@@ -1,5 +1,7 @@
 import { Router } from 'express';
+import jwt from 'jsonwebtoken';
 import { socialLoginService, userService } from '../services';
+import { convertExpToMaxAge } from '../utils/jwt';
 
 const socialLoginRouter = Router();
 
@@ -21,22 +23,37 @@ socialLoginRouter.get('/kakao/callback', async (req, res, next) => {
 
     const userData = await socialLoginService.kakaoLogin(code);
     const { isRegister, email, fullName } = userData;
-    // 로그인했던 적이 있으면 바로 로그인을 시켜주고 회원가입 절차는 생략!
+    const password = process.env.KAKAO_CLIENT_SECRET || 'kakao';
+    // 로그인했던 적이 있으면 바로 로그인을 시켜주고 회원가입 절차는 생략함.
     if (!isRegister) {
       await userService.addUser({
         email,
         fullName,
-        password: 'kakao',
+        password,
       });
     }
 
-    const userToken = await userService.getUserToken({
+    const { accessToken, refreshToken } = await userService.getUserToken({
       email,
-      password: 'kakao',
+      password,
     });
 
-    // 프론트 단에는 최종적으로 userToken을 넘겨줘야함.
-    res.status(200).json(userToken);
+    const accessExp = jwt.decode(accessToken).exp;
+    const refreshExp = jwt.decode(refreshToken).exp;
+
+    const accessMaxAge = convertExpToMaxAge(accessExp);
+    const refreshMaxAge = convertExpToMaxAge(refreshExp);
+
+    // jwt 토큰을 쿠키에 저장함.
+    res.cookie('accessToken', accessToken, {
+      maxAge: accessMaxAge,
+    });
+    res.cookie('refreshToken', refreshToken, {
+      maxAge: refreshMaxAge,
+    });
+    res.cookie('email', email);
+
+    res.status(200).redirect('/');
   } catch (err) {
     next(err);
   }
