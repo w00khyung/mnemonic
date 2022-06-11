b8842d0d9a4200b1379e2d473c3a0e5bec9dc948
[SW2/부산]오원빈 <ohwon99@gmail.com>
2022-06-11 01:47:38 +0000

    Revert "Merge branch 'feature-mypage-change' into 'master'"
    This reverts merge request !78

--- src/views/admin/admin.js
+++ src/views/admin/admin.js
@@ -4,3 +4,8 @@ import { getCookie } from '/useful-functions.js';
 
 navRender();
 pageScroll();
+
+if (getCookie.getItem('email') !== 'manager@gmail.com') {
+  alert('관리자만 접근이 가능합니다.');
+  window.location.href = '/login';
+}
